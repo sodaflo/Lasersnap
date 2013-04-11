bc8090655f1d113a6271fd28c54e3ea422c1c088
sodaflo <derecht@googlemail.com>
2013-04-11 14:35:54 +0200

    added Arduinocontroller

---
+++ Arduino/Lasercontroller/Lasercontroller.ino
@@ -0,0 +1,177 @@
+/*
+Arduino as a Controller for a Lasercutter made out of 2 Scanner
+
+ Works together with another Programm called Lasersnap
+ 
+
+ by Florian Albrecht
+ 
+ */
+
+#include <Stepper.h>
+
+#define Laserpin 3 //The Pin on which the Relaise for the Laser is attached
+
+
+#define motorSteps 200     // change this depending on the number of steps
+#define motorStepsz 200
+// per revolution of your motor
+#define motorPin1 12
+#define motorPin2 11
+#define motorPin3 10
+#define motorPin4 9
+#define ledPin 13
+#define motorPin1z 7
+#define motorPin2z 6
+#define motorPin3z 5
+#define motorPin4z 4
+// initialize of the Stepper library:
+Stepper myStepperx(200, 12,11,10,9); 
+Stepper mySteppery(200, 4,5,6,7);
+void setup() {
+  // set the motor speed at 60 RPMS:
+  myStepperx.setSpeed(80);
+  mySteppery.setSpeed(80);
+  // Initialize the Serial port:
+  Serial.begin(9600);
+
+  // set up the LED pin:
+  pinMode(ledPin, OUTPUT);
+  // blink the LED:
+  pinMode(Laserpin, OUTPUT);
+  blink(3);
+}
+
+int nowx = 0, nowy = 0; //position of Laser
+
+
+float xyratio = 1.4; //ratio of gears of y and x
+int t = 1;
+double get = 0;
+
+
+void loop() {
+
+  if(Serial.available()>0){
+    int coordx, coordy; //receiving coordinates
+    boolean status;
+    int stat = Serial.read();
+    if(stat == 79){ //if first character ist O Laser will not be on
+      status = false;
+    }
+    if(stat == 80){ //if first character ist P Laser will be turned on
+      status = true;
+    }
+    coordx = getint();
+    coordy = getint();
+    Serial.print("x:");
+    Serial.print(coordx);
+    Serial.print("  y:");
+    Serial.println(coordy);
+    movelaser(coordx, coordy, status);
+  }
+}
+
+void movelaser(int x, int y, boolean ison){
+  int movex, movey;
+  movex = x - nowx;
+  movey = y - nowy;
+  int directionx, directiony;
+  if(ison){
+    digitalWrite(Laserpin, HIGH);
+  }
+  if(movex > 0){
+    directionx = 1;
+   }
+   else {
+    directionx = -1;
+    movex *= -1;
+  }
+  if(movey > 0){
+    directiony = 1;
+    }
+    else{
+      directiony = -1;
+      movey *= -1;
+    }
+  int steppedx = 0;
+  int steppedy = 0;
+  if(movex > movey){
+    myStepperx.step(directionx);
+    steppedx++;
+    nowx += directionx;
+    }
+    else{
+      mySteppery.step(directiony);
+      steppedy++;
+      nowy += directiony;
+  }
+  while(steppedx != movex && steppedy != movey){
+    if(steppedx/movex > steppedy){
+      myStepperx.step(directionx);
+      steppedx++;
+      nowx += directionx;
+      }
+      else{
+        mySteppery.step(directiony);
+        steppedy++;
+        nowy += directiony;
+     }
+  }
+  if(ison){
+    digitalWrite(Laserpin, LOW);
+  }
+}
+
+// Blink the reset LED:
+void blink(int howManyTimes) {
+  int i;
+  for (i=0; i< howManyTimes; i++) {
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);
+  }
+}
+
+
+double getint(){
+  boolean minus = false;
+  double l = 1;
+  double number = 0;
+  if(Serial.available() > 0){
+    int get[20];
+    int i = 0;
+    while(Serial.available() > 0){
+      get[i] = Serial.read() - 48;
+      if(get[i] < (124 - 48)){
+      if(get[i] == -3){
+        i--;
+        minus = true;
+      }
+      i++;
+      delay(30);
+    }
+    }
+    i--;
+    i--;
+    if(i > 0){
+      int a = i;
+      while(a > 0){
+        l *= 10;
+        a--;
+      }
+    }      
+    double c = 1;
+    while(i>=0){
+      number = number + get[i]*c;
+      c *= 10;
+      i--;
+    }
+  }
+  if(minus){
+    number *= -1;
+  }
+  return number;
+}
+
---
+++ sendingdata.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+
+//#include <sys/time.h>
+#include <sys/types.h>
+
+
+#define TERM_DEVICE "/dev/ttyACM0"
+#define TERM_SPEED B9600
+
+int main(){
+	int fd, old_flags;
+	ssize_t length;
+	char buffer[16];
+	struct termios term_attr;
+	fd_set input_fdset;
+
+	if ((fd = open(TERM_DEVICE, O_RDWR)) == -1){
+		perror("terminal: Can't open device " TERM_DEVICE);
+		return(1);
+		}
+
+	if (tcgetattr(fd, &term_attr) != 0){
+		perror("termina: tcgetattr() failed");
+		return(1);
+		}
+	term_attr.c_cflag = TERM_SPEED | CS8 | CRTSCTS
+						| CLOCAL | CREAD;
+	term_attr.c_iflag = 0;
+	term_attr.c_oflag = OPOST | ONLCR;
+	term_attr.c_lflag = 0;
+	if (tcsetattr(fd, TCSAFLUSH, &term_attr) != 0){
+		perror("terminal: tcsetattr() failed");
+	}
+		//Std.-Eingabe anpassen
+	if (tcgetattr(STDIN_FILENO, &term_attr) != 0){
+		perror("terminal: tcgetattr() failed");
+		return(1);
+		}
+		//alte Einstellung sichern
+	old_flags = term_attr.c_lflag;
+	term_attr.c_lflag &= ~(ICANON | ECHO);
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_attr) != 0){
+		perror("terminal:tcsetattr() failed");
+		}
+	while (1){
+		FD_ZERO(&input_fdset);
+		FD_SET(STDIN_FILENO, &input_fdset);
+		FD_SET(fd, &input_fdset);
+		if (select(fd+1, &input_fdset, NULL, NULL, NULL) == -1){
+			perror("terminal: select() failed");
+			}
+		if ((FD_ISSET(STDIN_FILENO, &input_fdset)) == -1){
+			perror("terminal: read() failed");
+			}
+			else if (buffer[0] == '\33'){
+				break;
+				}
+			else{
+				write(fd, buffer, length);
+				}
+		if (FD_ISSET(fd, &input_fdset)){
+			if ((length = read(fd, buffer, 16)) == -1){
+				perror("terminal: read() failed");
+			}
+			else {
+				write(STDOUT_FILENO, buffer, length);
+				}
+			}
+		}
+				//Std.-Eingabe wie vorher
+	term_attr.c_lflag = old_flags;
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &term_attr) != 0){
+	perror("terminal: tcsetattr() failed");
+	}
+
+	printf("Aborted. \n");
+	close(fd);
+	return(0);
+}
